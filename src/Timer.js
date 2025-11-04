a30cca2b3e9003f46104a6cc7b92e458393e1c57
agomezi <alex.gomezd02@gmail.com>
2025-11-04 00:41:10 -0500

    Timer Setup

---
+++ src/Timer.js
@@ -0,0 +1,30 @@
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+import PlayButton from "./PlayButton";
+import PauseButton from "./PauseButton";
+
+const red = "#f54e4e";
+const green = "4aec8c";
+
+function Timer() {
+  return (
+    <div>
+      <CircularProgressbar
+        value={60}
+        text={`60%`}
+        styles={buildStyles({
+          textColor: "#fff",
+          pathColor: red,
+          tailColor: "rgba(255,255,255, .2)",
+        })}
+      />
+      <div style={{ marginTop: "20px" }}>
+        <PlayButton />
+        <PauseButton />
+      </div>
+      <div style={{ marginTop: "20px" }}></div>
+    </div>
+  );
+}
+
+export default Timer;
