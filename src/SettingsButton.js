27dcc323404e9062551d4ebd41bffcf4f30f4501
agomezi <alex.gomezd02@gmail.com>
2025-11-04 17:55:09 -0500

    Changed Styling for buttons

--- src/SettingsButton.js
+++ src/SettingsButton.js
@@ -1,6 +1,6 @@
 function SettingsButton(props) {
   return (
-    <button {...props}>
+    <button {...props} className={"with-text"}>
       <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 24 24"
