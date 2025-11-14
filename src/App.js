ce305b9fb1e107eaf1907cd2b81ae2cca3d56512
agomezi <alex.gomezd02@gmail.com>
2025-11-13 23:42:19 -0500

    Added in sliders and SettingsContext

--- src/App.js
+++ src/App.js
@@ -2,11 +2,23 @@ import "./App.css";
 import Timer from "./Timer";
 import Settings from "./Settings";
 import { useState } from "react";
+import SettingsContext from "./SettingsContext";
 
 function App() {
   const [showSettings, setShowSettings] = useState(true);
 
-  return <main>{showSettings ? <Settings /> : <Timer />}</main>;
+  return (
+    <main>
+      <SettingsContext.Provider
+        value={{
+          workMinutes: 45,
+          breakMinutes: 15,
+        }}
+      >
+        {showSettings ? <Settings /> : <Timer />}
+      </SettingsContext.Provider>
+    </main>
+  );
 }
 
 export default App;
--- src/Settings.js
+++ src/Settings.js
@@ -1,21 +1,28 @@
 import Slider from "rc-slider";
 import "rc-slider/assets/index.css";
 import "./Slider.css";
+import SettingsContext from "./SettingsContext";
+import { useContext } from "react";
 
 function Settings() {
+  const settingsInfo = useContext(SettingsContext);
   return (
     <div style={{ textAlign: "left" }}>
-      <label>Work Minutes:</label>
+      <label>Work Minutes: {settingsInfo.workMinutes}</label>
       <Slider
         className={"slider"}
-        //thumbClassName={"thumb"}
-        //trackClassName={"track"}
-        value={45}
+        value={settingsInfo.workMinutes}
         min={1}
         max={120}
       />
 
-      <label>Break Minutes:</label>
+      <label>Break Minutes: {settingsInfo.breakMinutes}</label>
+      <Slider
+        className={"slider green"}
+        value={settingsInfo.breakMinutes}
+        min={1}
+        max={120}
+      />
     </div>
   );
 }
