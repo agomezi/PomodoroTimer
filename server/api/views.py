08d736b5d3b3aa71af20321a378fa904a1ccc1a6
agomezi <alex.gomezd02@gmail.com>
2026-02-17 19:11:13 -0500

    Adding Views.py importing serializers Added UserSettingsSerializer that was previously missing

--- server/api/views.py
+++ server/api/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics, status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.response import Response
+from django.utils import timezone
+from datetime import date
 
-# Create your views here.
+
+from .serializers import (
+    RegisterSerializer,
+    UserSettingsSerializer,
+    UserSerializer,
+    DailyProgresSerializer,
+)
+
+from .models import UserSettings, DailyProgress
+
+
